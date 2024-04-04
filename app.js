463ec76ebd902257ccba737f7c2809d4746fd30d
Steven Dickerson <poddocdan@gmail.com>
2024-04-04 07:45:53 -0700

    cleaned up the comments on ex3

--- app.js
+++ app.js
@@ -18,21 +18,54 @@ printOdds(3);
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 function checkAge(userName, age) {
+  if (!userName) {
+    console.log("Please provide a valid username");
+    return;
+  }
+  if (!age) {
+    console.log("Please provide a valid age");
+    return;
+  }
   const aboveSixteen = `Congrats ${userName}, you can drive!`;
   const belowSixteen = `Sorry ${userName}, but you need to wait until you're 16`;
   if (age < 16) {
     console.log(belowSixteen);
-  } else {
+  } else if (age >= 16) {
     console.log(aboveSixteen);
+  } else {
+    console.log("Invalid age");
   }
 }
 checkAge("Dan", "16");
+// if there is nothing in the age column then 'age' is undefined which makes age <16 false so it auto runs aboveSixteen so you need age >=16
 
 // Exercise 3 Section
 console.log("EXERCISE 3:\n==========\n");
-function cartesian(x, y) {
-    if ()
+// break this down from lease likely to most likely
+// look at a cartesian plane and say "ok what does x and y have to be greater than or less than to be in a certain quadrant"
+
+function getQuadrant(x, y) {
+  if (x === undefined || y === undefined) {
+    console.log("Invalid (x, y) location");
+    return null;
+  }
+  if (x === 0 && y === 0) {
+    console.log("Origin Point");
+  } else if (x == 0) {
+    console.log("Y-axis");
+  } else if (y === 0) {
+    console.log("X-axis");
+  } else if (x > 0 && y > 0) {
+    console.log("Quadrant 1");
+  } else if (x < 0 && y > 0) {
+    console.log("Quadrant 2");
+  } else if (x < 0 && y < 0) {
+    console.log("Quadrant 3");
+  } else if (x < 0 && y > 0) {
+    console.log("Quadrant 4");
+  }
 }
+getQuadrant(-5, -3);
 
 // // Exercise 4 Section
 console.log("EXERCISE 4:\n==========\n");
